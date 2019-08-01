71bf05486e1e5cfb790e77e6981e93bad8535eec
Yulin(Leonard) Zhang <yuliz12@uci.edu>
2019-08-01 17:04:03 +0800

    Create Account.java

---
+++ src/java/bean/Account.java
@@ -0,0 +1,11 @@
+package bean;
+
+public class Account {
+	public String id;
+	public String subnumber;
+	public String money;
+	public String moneytype;
+	public String savetype;
+	public String time;
+
+}
